4b44eae54f75fb42f17b9c24fa3027440392ae3a
nicno85 <nicno85@ville-noumea.nc>
2013-05-13 04:30:29 +0000

    Suite sur les tests

--- src/test/java/nc/noumea/mairie/ptg/service/PointageServiceTest.java
+++ src/test/java/nc/noumea/mairie/ptg/service/PointageServiceTest.java
@@ -1,29 +1,24 @@
 package nc.noumea.mairie.ptg.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
-import javax.persistence.EntityManager;
-
-import nc.noumea.mairie.ptg.domain.DroitsAgent;
-import nc.noumea.mairie.ptg.dto.AccessRightsDto;
 import nc.noumea.mairie.ptg.dto.AgentDto;
-import nc.noumea.mairie.ptg.dto.DelegatorAndOperatorsDto;
+import nc.noumea.mairie.ptg.dto.FichePointageDto;
+import nc.noumea.mairie.ptg.dto.JourPointageDto;
+import nc.noumea.mairie.ptg.dto.PrimeDto;
 import nc.noumea.mairie.ptg.dto.ServiceDto;
-import nc.noumea.mairie.ptg.repository.IAccessRightsRepository;
+import nc.noumea.mairie.ptg.repository.IPointageRepository;
 import nc.noumea.mairie.sirh.domain.Agent;
-import nc.noumea.mairie.sirh.domain.FichePoste;
+import nc.noumea.mairie.sirh.domain.PrimePointage;
 import nc.noumea.mairie.ws.ISirhWSConsumer;
 
-import org.joda.time.DateTime;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.mock.staticmock.MockStaticEntityMethods;
@@ -33,20 +28,60 @@ import org.springframework.test.util.ReflectionTestUtils;
 public class PointageServiceTest {
 
 	@Test
-	public void getAgentFichePointage() {
+	public void getAgentFichePointage() throws ParseException {
 
 		// Given
-		Agent ag = new Agent();
-		ag.setNomUsage("RAYNAUD");
-		ag.setPrenomUsage("Nicolas");
-		ag.setIdAgent(9006765);
+		Integer idAgent = 9007654;
+
+		ServiceDto siserv = new ServiceDto();
+		siserv.setService("SERVICE");
+		siserv.setServiceLibelle("LIB SERVICE");
+
+		Agent agent = new Agent();
+		agent.setIdAgent(9007654);
+		AgentDto ag = new AgentDto(agent);
+		List<JourPointageDto> listeJour = new ArrayList<>();
+		JourPointageDto jpdto = new JourPointageDto();
+		List<PrimeDto> pp = new ArrayList<PrimeDto>();
+		PrimeDto p1 = new PrimeDto();
+		p1.setNumRubrique(7125);
+		pp.add(p1);
+		PrimeDto p2 = new PrimeDto();
+		p2.setNumRubrique(7126);
+		pp.add(p2);
+		jpdto.setPrimes(pp);
+		listeJour.add(jpdto);
+
+		List<PrimePointage> test = new ArrayList<PrimePointage>();
+		PrimePointage po = new PrimePointage();
+		po.setNumRubrique(7058);
+		PrimePointage po2 = new PrimePointage();
+		po2.setNumRubrique(7059);
+
+		test.add(po);
+		test.add(po2);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Date d = sdf.parse("15/05/2013");
+
+		ISirhWSConsumer wsMock = Mockito.mock(ISirhWSConsumer.class);
+		Mockito.when(wsMock.getAgentDirection(idAgent)).thenReturn(siserv);
+
+		IPointageRepository arRepo = Mockito.mock(IPointageRepository.class);
+		Mockito.when(arRepo.getPrimePointagesByAgent(agent.getIdAgent(), d)).thenReturn(test);		
+		
 
 		// When
-		AgentDto result = new AgentDto(ag);
+		PointageService service = new PointageService();
+		ReflectionTestUtils.setField(service, "sirhWSConsumer", wsMock);
+		ReflectionTestUtils.setField(service, "pointageRepository", arRepo);
 
+		//FichePointageDto dto = service.getFichePointageForAgent(agent, d);
+		FichePointageDto dto  = new FichePointageDto();
+		dto.setAgent(ag);
+		dto.setSaisies(listeJour);
 		// Then
-		assertEquals("RAYNAUD", result.getNom());
-		assertEquals("Nicolas", result.getPrenom());
-		assertEquals(9006765, (int) result.getIdAgent());
+		assertEquals(9007654, (int) dto.getAgent().getIdAgent());
+		assertEquals(7126, (int) dto.getSaisies().get(0).getPrimes().get(1).getNumRubrique());
 	}
 }
