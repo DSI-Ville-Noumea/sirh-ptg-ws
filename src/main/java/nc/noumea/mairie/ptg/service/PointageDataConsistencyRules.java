bf5753abbf95a226df44f709b24d2b12f3bc8d22
rayni84 <rayni84@ville-noumea.nc>
2013-05-29 01:24:21 +0000

    RG-Pointages-17 pour les congés SPCONG

--- src/main/java/nc/noumea/mairie/ptg/service/PointageDataConsistencyRules.java
+++ src/main/java/nc/noumea/mairie/ptg/service/PointageDataConsistencyRules.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import nc.noumea.mairie.domain.Spcong;
 import nc.noumea.mairie.domain.Sprirc;
 import nc.noumea.mairie.ptg.domain.Pointage;
 import nc.noumea.mairie.ptg.repository.IMairieRepository;
@@ -31,8 +32,8 @@ public class PointageDataConsistencyRules implements IPointageDataConsistencyRul
 		
 		for (Sprirc recup : recups) {
 
-			DateTime recupDateDeb = GetDateDebut(recup);
-			DateTime recupDateFin = GetDateFin(recup);
+			DateTime recupDateDeb = GetDateDebut(recup.getId().getDatdeb(), recup.getId().getCodem1());
+			DateTime recupDateFin = GetDateFin(recup.getDatfin(), recup.getCodem2());
 			
 			for (Pointage ptg : pointages) {
 				DateTime ptgTime = new DateTime(ptg.getDateDebut());
@@ -51,31 +52,54 @@ public class PointageDataConsistencyRules implements IPointageDataConsistencyRul
 		return errors;
 	}
 
-	protected DateTime GetDateFin(Sprirc recup) {
-		DateTime recupDateFin = new DateTime(helperService.getDateFromMairieInteger(recup.getDatfin()));
-		if (recup.getId().getCodem1().equals(1))
+	@Override
+	public List<String> checkSpcongConge(Integer idAgent, Date dateLundi, List<Pointage> pointages) {
+		
+		List<String> errors = new ArrayList<String>();
+		
+		Date end = new DateTime(dateLundi).plusDays(7).toDate();
+		
+		List<Spcong> conges = mairieRepository.getListCongeBetween(idAgent, dateLundi, end);
+		
+		for (Spcong cg : conges) {
+
+			DateTime recupDateDeb = GetDateDebut(cg.getId().getDatdeb(), cg.getCodem1());
+			DateTime recupDateFin = GetDateFin(cg.getDatfin(), cg.getCodem2());
+			
+			for (Pointage ptg : pointages) {
+				DateTime ptgTime = new DateTime(ptg.getDateDebut());
+				DateTime ptgTimeEnd = new DateTime(ptg.getDateFin());
+				
+				if (ptgTime.isAfter(recupDateDeb) && ptgTime.isBefore(recupDateFin))
+					errors.add(String.format("%s : L'agent est en congé sur cette période.", 
+							ptgTime.toString("dd/MM/yyyy HH:mm")));
+				else if (ptgTimeEnd.isAfter(recupDateDeb) && ptgTimeEnd.isBefore(recupDateFin)) {
+					errors.add(String.format("%s : L'agent est en congé sur cette période.", 
+							ptgTimeEnd.toString("dd/MM/yyyy HH:mm")));
+				}
+			}
+		}
+		
+		return errors;
+	}
+	
+	protected DateTime GetDateFin(Integer dateFin, Integer codem2) {
+		DateTime recupDateFin = new DateTime(helperService.getDateFromMairieInteger(dateFin));
+		if (codem2.equals(1))
 			recupDateFin = recupDateFin.plusMinutes(690); // 11h30
 		else
 			recupDateFin = recupDateFin.plusMinutes(930); // 15h30
 		return recupDateFin;
 	}
 
-	protected DateTime GetDateDebut(Sprirc recup) {
-		DateTime recupDateDeb = new DateTime(helperService.getDateFromMairieInteger(recup.getId().getDatdeb()));
-		if (recup.getId().getCodem1().equals(1))
+	protected DateTime GetDateDebut(Integer dateDeb, Integer codem1) {
+		DateTime recupDateDeb = new DateTime(helperService.getDateFromMairieInteger(dateDeb));
+		if (codem1.equals(1))
 			recupDateDeb = recupDateDeb.plusMinutes(435); // 7h15
 		else
 			recupDateDeb = recupDateDeb.plusMinutes(720); // 12h00
 		return recupDateDeb;
 	}
 	
-	@Override
-	public List<String> checkSpcongConge(Integer idAgent, Date dateLundi, List<Pointage> pointages) {
-		
-		List<String> errors = new ArrayList<String>();
-		
-		
-		
-		return errors;
-	}
+	
 }
--- src/test/java/PointageDataConsistencyRules/PointageDataConsistencyRulesTest.java
+++ src/test/java/PointageDataConsistencyRules/PointageDataConsistencyRulesTest.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import nc.noumea.mairie.domain.Spcong;
+import nc.noumea.mairie.domain.SpcongId;
 import nc.noumea.mairie.domain.Sprirc;
 import nc.noumea.mairie.domain.SprircId;
 import nc.noumea.mairie.ptg.domain.Pointage;
@@ -189,5 +191,177 @@ public class PointageDataConsistencyRulesTest {
 		// Then
 		assertEquals(1, result.size());
 		assertEquals("22/05/2013 11:00 : L'agent est en récupération sur cette période.", result.get(0));
+	}
+	
+	@Test
+	public void checkSpcongConge_NoSpcong_NoError() {
+		
+		// Given
+		Integer idAgent = 9005138;
+		Date dateLundi = new DateTime(2013, 5, 20, 0, 0, 0).toDate();
+		List<Pointage> ptgs = new ArrayList<Pointage>();
+		
+		IMairieRepository mRepo = Mockito.mock(IMairieRepository.class);
+		Mockito.when(mRepo.getListCongeBetween(idAgent, dateLundi, new DateTime(dateLundi).plusDays(7).toDate())).thenReturn(new ArrayList<Spcong>());
+		
+		PointageDataConsistencyRules service = new PointageDataConsistencyRules();
+		ReflectionTestUtils.setField(service, "mairieRepository", mRepo);
+		
+		// When
+		List<String> result = service.checkSpcongConge(idAgent, dateLundi, ptgs);
+		
+		// Then
+		assertEquals(0, result.size());
+	}
+	
+	@Test
+	public void checkSpcongConge_1Spcong_NoPointageThatDay_NoError() {
+		
+		// Given
+		Integer idAgent = 9005138;
+		Date dateLundi = new DateTime(2013, 5, 20, 0, 0, 0).toDate();
+		List<Pointage> ptgs = new ArrayList<Pointage>();
+		
+		Spcong sp = new Spcong();
+		sp.setId(new SpcongId(5138, 20130521, 0, 0));
+		sp.setCodem1(1);
+		sp.setDatfin(20130521);
+		sp.setCodem2(1);
+		
+		IMairieRepository mRepo = Mockito.mock(IMairieRepository.class);
+		Mockito.when(mRepo.getListCongeBetween(idAgent, dateLundi, new DateTime(dateLundi).plusDays(7).toDate())).thenReturn(Arrays.asList(sp));
+		
+		HelperService hS = Mockito.mock(HelperService.class);
+		Mockito.when(hS.getDateFromMairieInteger(20130521)).thenReturn(new DateTime(2013, 5, 21, 0, 0, 0).toDate());
+		
+		PointageDataConsistencyRules service = new PointageDataConsistencyRules();
+		ReflectionTestUtils.setField(service, "mairieRepository", mRepo);
+		ReflectionTestUtils.setField(service, "helperService", hS);
+		
+		// When
+		List<String> result = service.checkSpcongConge(idAgent, dateLundi, ptgs);
+		
+		// Then
+		assertEquals(0, result.size());
+	}
+	
+	@Test
+	public void checkSpcongConge_1Spcong_1PointageThatDay_ReturnError() {
+		
+		// Given
+		Integer idAgent = 9005138;
+		Date dateLundi = new DateTime(2013, 5, 20, 0, 0, 0).toDate();
+		
+		Pointage p1 = new Pointage();
+		p1.setDateLundi(dateLundi);
+		p1.setDateDebut(new DateTime(2013, 5, 21, 7, 0, 0).toDate());
+		p1.setDateFin(new DateTime(2013, 5, 21, 9, 0, 0).toDate());
+		p1.setType(new RefTypePointage());
+		p1.getType().setIdRefTypePointage(RefTypePointageEnum.H_SUP.getValue());
+		List<Pointage> ptgs = Arrays.asList(p1);
+		
+		// en recup le matin du 21/05/2013
+		Spcong sp = new Spcong();
+		sp.setId(new SpcongId(5138, 20130521, 0, 0));
+		sp.setCodem1(1);
+		sp.setDatfin(20130521);
+		sp.setCodem2(1);
+		
+		IMairieRepository mRepo = Mockito.mock(IMairieRepository.class);
+		Mockito.when(mRepo.getListCongeBetween(idAgent, dateLundi, new DateTime(dateLundi).plusDays(7).toDate())).thenReturn(Arrays.asList(sp));
+		
+		HelperService hS = Mockito.mock(HelperService.class);
+		Mockito.when(hS.getDateFromMairieInteger(20130521)).thenReturn(new DateTime(2013, 5, 21, 0, 0, 0).toDate());
+		
+		PointageDataConsistencyRules service = new PointageDataConsistencyRules();
+		ReflectionTestUtils.setField(service, "mairieRepository", mRepo);
+		ReflectionTestUtils.setField(service, "helperService", hS);
+		
+		// When
+		List<String> result = service.checkSpcongConge(idAgent, dateLundi, ptgs);
+		
+		// Then
+		assertEquals(1, result.size());
+		assertEquals("21/05/2013 09:00 : L'agent est en congé sur cette période.", result.get(0));
+	}
+	
+	@Test
+	public void checkSpcongConge_1Spcong_1PointageThatDayBeforeRecup_ReturnNoError() {
+		
+		// Given
+		Integer idAgent = 9005138;
+		Date dateLundi = new DateTime(2013, 5, 20, 0, 0, 0).toDate();
+		
+		Pointage p1 = new Pointage();
+		p1.setDateLundi(dateLundi);
+		p1.setDateDebut(new DateTime(2013, 5, 21, 4, 0, 0).toDate());
+		p1.setDateFin(new DateTime(2013, 5, 21, 5, 0, 0).toDate());
+		p1.setType(new RefTypePointage());
+		p1.getType().setIdRefTypePointage(RefTypePointageEnum.H_SUP.getValue());
+		List<Pointage> ptgs = Arrays.asList(p1);
+		
+		// en recup le matin du 21/05/2013
+		Spcong sp = new Spcong();
+		sp.setId(new SpcongId(5138, 20130521, 0, 0));
+		sp.setCodem1(1);
+		sp.setDatfin(20130521);
+		sp.setCodem2(1);
+		
+		IMairieRepository mRepo = Mockito.mock(IMairieRepository.class);
+		Mockito.when(mRepo.getListCongeBetween(idAgent, dateLundi, new DateTime(dateLundi).plusDays(7).toDate())).thenReturn(Arrays.asList(sp));
+		
+		HelperService hS = Mockito.mock(HelperService.class);
+		Mockito.when(hS.getDateFromMairieInteger(20130521)).thenReturn(new DateTime(2013, 5, 21, 0, 0, 0).toDate());
+		
+		PointageDataConsistencyRules service = new PointageDataConsistencyRules();
+		ReflectionTestUtils.setField(service, "mairieRepository", mRepo);
+		ReflectionTestUtils.setField(service, "helperService", hS);
+		
+		// When
+		List<String> result = service.checkSpcongConge(idAgent, dateLundi, ptgs);
+		
+		// Then
+		assertEquals(0, result.size());
+	}
+	
+	@Test
+	public void checkSpcongConge_1Spcong_1PointageInsidePeriod_ReturnError() {
+		
+		// Given
+		Integer idAgent = 9005138;
+		Date dateLundi = new DateTime(2013, 5, 20, 0, 0, 0).toDate();
+		
+		Pointage p1 = new Pointage();
+		p1.setDateLundi(dateLundi);
+		p1.setDateDebut(new DateTime(2013, 5, 22, 11, 0, 0).toDate());
+		p1.setDateFin(new DateTime(2013, 5, 22, 12, 0, 0).toDate());
+		p1.setType(new RefTypePointage());
+		p1.getType().setIdRefTypePointage(RefTypePointageEnum.H_SUP.getValue());
+		List<Pointage> ptgs = Arrays.asList(p1);
+		
+		// en recup le matin du 21/05/2013
+		Spcong sp = new Spcong();
+		sp.setId(new SpcongId(5138, 20130515, 0, 0));
+		sp.setCodem1(2);
+		sp.setDatfin(20130522);
+		sp.setCodem2(2);
+		
+		IMairieRepository mRepo = Mockito.mock(IMairieRepository.class);
+		Mockito.when(mRepo.getListCongeBetween(idAgent, dateLundi, new DateTime(dateLundi).plusDays(7).toDate())).thenReturn(Arrays.asList(sp));
+		
+		HelperService hS = Mockito.mock(HelperService.class);
+		Mockito.when(hS.getDateFromMairieInteger(20130515)).thenReturn(new DateTime(2013, 5, 15, 0, 0, 0).toDate());
+		Mockito.when(hS.getDateFromMairieInteger(20130522)).thenReturn(new DateTime(2013, 5, 22, 0, 0, 0).toDate());
+		
+		PointageDataConsistencyRules service = new PointageDataConsistencyRules();
+		ReflectionTestUtils.setField(service, "mairieRepository", mRepo);
+		ReflectionTestUtils.setField(service, "helperService", hS);
+		
+		// When
+		List<String> result = service.checkSpcongConge(idAgent, dateLundi, ptgs);
+		
+		// Then
+		assertEquals(1, result.size());
+		assertEquals("22/05/2013 11:00 : L'agent est en congé sur cette période.", result.get(0));
 	}
 }
