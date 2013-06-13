ed7191f2d8b12eb0194d48b39a69f52d4f59fe20
rayni84 <rayni84@ville-noumea.nc>
2013-06-13 05:34:28 +0000

    fixing scripts for 0.4 (adding drop column script to be executed at the end of deployment)

--- src/site/resources/scripts/0.4-3-PTG_ADM.drop.columns.sql
+++ src/site/resources/scripts/0.4-3-PTG_ADM.drop.columns.sql
@@ -2,4 +2,5 @@
 -- connecte en PTG_ADM
 ----------------------------------------------------------------
 
-ALTER TABLE PTG_ADM.PTG_ETAT_POINTAGE ADD (ID_AGENT NUMBER(38,0));
+alter table PTG_ADM.PTG_DROIT drop column CODE_SERVICE;
+alter table PTG_ADM.PTG_DROITS_AGENT drop column ID_DROIT;
