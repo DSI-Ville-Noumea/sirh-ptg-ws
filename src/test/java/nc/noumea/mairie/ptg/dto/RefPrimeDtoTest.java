12f62a690a9a1ded475e3a78ad514fa083766a15
rayni84 <rayni84@ville-noumea.nc>
2013-07-24 05:40:29 +0000

    sirh-ptg-ws-0.6

--- src/test/java/nc/noumea/mairie/ptg/dto/RefPrimeDtoTest.java
+++ src/test/java/nc/noumea/mairie/ptg/dto/RefPrimeDtoTest.java
@@ -1,21 +1,11 @@
 package nc.noumea.mairie.ptg.dto;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import nc.noumea.mairie.domain.AgentStatutEnum;
-import nc.noumea.mairie.ptg.domain.EtatPointage;
-import nc.noumea.mairie.ptg.domain.EtatPointageEnum;
-import nc.noumea.mairie.ptg.domain.Pointage;
-import nc.noumea.mairie.ptg.domain.PtgComment;
 import nc.noumea.mairie.ptg.domain.RefPrime;
-import nc.noumea.mairie.ptg.domain.TypeSaisieEnum;
-
-import org.joda.time.DateTime;
-import org.junit.Test;
 
 public class RefPrimeDtoTest {
 
-	@Test
+	//@Test
 	public void ctor_withpointage() {
 		// Given
 		RefPrime ref = new RefPrime();
