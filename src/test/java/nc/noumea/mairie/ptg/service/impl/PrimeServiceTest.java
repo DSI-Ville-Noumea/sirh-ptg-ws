182380df74ae67130b5502e11ac7c195d621e249
levch80 <levch80@ville-noumea.nc>
2013-09-05 03:03:57 +0000

    #3997 Added test for PrimeService class

--- src/test/java/nc/noumea/mairie/ptg/service/impl/PrimeServiceTest.java
+++ src/test/java/nc/noumea/mairie/ptg/service/impl/PrimeServiceTest.java
@@ -20,10 +20,8 @@ public class PrimeServiceTest {
 	@Test
 	public void getPrimeListForAgent() {
 		// Given
-		List<RefPrime> result = Mockito.mock(IPointageRepository.class)
-				.getRefPrimesListForAgent(AgentStatutEnum.F);
-		List<RefPrimeDto> result2 = Mockito.mock(IPrimeService.class)
-				.getPrimeListForAgent(AgentStatutEnum.F);
+		List<RefPrime> result = Mockito.mock(IPointageRepository.class).getRefPrimesListForAgent(AgentStatutEnum.F);
+		List<RefPrimeDto> result2 = Mockito.mock(IPrimeService.class).getPrimeListForAgent(AgentStatutEnum.F);
 
 		// Then
 		testList(result, result2);
@@ -32,10 +30,8 @@ public class PrimeServiceTest {
 	@Test
 	public void getPrimeList() {
 		// Given
-		List<RefPrime> result = Mockito.mock(IPointageRepository.class)
-				.getRefPrimesList();
-		List<RefPrimeDto> result2 = Mockito.mock(IPrimeService.class)
-				.getPrimeList();
+		List<RefPrime> result = Mockito.mock(IPointageRepository.class).getRefPrimesList();
+		List<RefPrimeDto> result2 = Mockito.mock(IPrimeService.class).getPrimeList();
 
 		// Then
 		testList(result, result2);
@@ -54,8 +50,7 @@ public class PrimeServiceTest {
 		List<RefPrime> listeRefPrime = Arrays.asList(p1, p2);
 
 		IPointageRepository arRepo = Mockito.mock(IPointageRepository.class);
-		Mockito.when(arRepo.getRefPrimesListWithNoRubr(noRubr)).thenReturn(
-				listeRefPrime);
+		Mockito.when(arRepo.getRefPrimesListWithNoRubr(noRubr)).thenReturn(listeRefPrime);
 
 		PrimeService service = new PrimeService();
 		ReflectionTestUtils.setField(service, "pointageRepository", arRepo);
@@ -84,14 +79,57 @@ public class PrimeServiceTest {
 		List<RefPrime> listeRefPrime = Arrays.asList(p1, p2);
 
 		IPointageRepository arRepo = Mockito.mock(IPointageRepository.class);
-		Mockito.when(arRepo.getRefPrimesListWithNoRubr(noRubr)).thenReturn(
-				listeRefPrime);
+		Mockito.when(arRepo.getRefPrimesListWithNoRubr(noRubr)).thenReturn(listeRefPrime);
 
 		PrimeService service = new PrimeService();
 		ReflectionTestUtils.setField(service, "pointageRepository", arRepo);
 
 		// When
 		List<RefPrimeDto> dto = service.getPrimes(noRubr);
+
+		// Then
+		assertEquals("TEST", dto.get(0).getLibelle());
+	}
+
+	@Test
+	public void getPrimeWithIdRefPrime() {
+		// Given
+		Integer idRefPrime = 123;
+		RefPrime p1 = new RefPrime();
+		p1.setIdRefPrime(idRefPrime);
+		p1.setLibelle("TEST");
+		RefPrime p2 = new RefPrime();
+		p2.setIdRefPrime(idRefPrime);
+		p2.setLibelle("TEST");
+		List<RefPrime> listeRefPrime = Arrays.asList(p1, p2);
+
+		IPointageRepository arRepo = Mockito.mock(IPointageRepository.class);
+		Mockito.when(arRepo.getRefPrimesListWithNoRubr(idRefPrime)).thenReturn(listeRefPrime);
+
+		PrimeService service = new PrimeService();
+		ReflectionTestUtils.setField(service, "pointageRepository", arRepo);
+
+		// When
+		List<RefPrimeDto> dto = service.getPrimes(idRefPrime);
+
+		// Then
+		assertEquals("TEST", dto.get(0).getLibelle());
+	}
+
+	@Test
+	public void getPrimesId() {
+		// Given
+		RefPrimeDto p1 = new RefPrimeDto();
+		p1.setLibelle("TEST");
+		RefPrimeDto p2 = new RefPrimeDto();
+		p2.setLibelle("TEST2");
+		List<RefPrimeDto> listeRefPrime = Arrays.asList(p1, p2);
+
+		IPrimeService arRepo = Mockito.mock(IPrimeService.class);
+		Mockito.when(arRepo.getPrimeList()).thenReturn(listeRefPrime);
+
+		// When
+		List<RefPrimeDto> dto = arRepo.getPrimeList();
 
 		// Then
 		assertEquals("TEST", dto.get(0).getLibelle());
