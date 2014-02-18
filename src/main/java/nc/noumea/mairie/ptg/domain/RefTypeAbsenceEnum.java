470a59f3b6c7df5b72c30a2d5cb36903f8f1e806
nicno85 <nicno85@ville-noumea.nc>
2014-02-18 22:13:20 +0000

    #6145 : PTG --> nouveau type d'absence

--- src/main/java/nc/noumea/mairie/ptg/domain/RefTypeAbsenceEnum.java
+++ src/main/java/nc/noumea/mairie/ptg/domain/RefTypeAbsenceEnum.java
@@ -1,30 +1,29 @@
 package nc.noumea.mairie.ptg.domain;
 
-
-public enum TypeAbsenceEnum {
+public enum RefTypeAbsenceEnum {
 
 	CONCERTEE(1), NON_CONCERTEE(2), IMMEDIATE(3);
-	
-	private int type;
-	
-	private TypeAbsenceEnum(int _type) {
+
+	private Integer type;
+
+	private RefTypeAbsenceEnum(Integer _type) {
 		type = _type;
 	}
-	
+
 	@Override
 	public String toString() {
 		return this.name();
 	}
-	
-	public int getValue() {
+
+	public Integer getValue() {
 		return type;
 	}
-	
-	public static TypeAbsenceEnum getTypeAbsenceEnum(Integer type) {
-		
+
+	public static RefTypeAbsenceEnum getRefTypeAbsenceEnum(Integer type) {
+
 		if (type == null)
 			return null;
-		
+
 		switch (type) {
 			case 1:
 				return CONCERTEE;
