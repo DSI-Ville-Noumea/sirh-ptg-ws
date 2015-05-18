909aedff78e53d2359df29a11de1f35c38dddacf
rebjo84 <rebjo84@ville-noumea.nc>
2015-05-18 05:15:12 +0000

    MER 1.30

--- src/main/java/nc/noumea/mairie/ptg/web/FicheController.java
+++ src/main/java/nc/noumea/mairie/ptg/web/FicheController.java
@@ -1,18 +1,13 @@
 package nc.noumea.mairie.ptg.web;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.util.Date;
 
 import nc.noumea.mairie.ptg.dto.FichePointageListDto;
-import nc.noumea.mairie.ptg.reporting.IReporting;
 import nc.noumea.mairie.ptg.service.IPointageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.lowagie.text.DocumentException;
-
 @Controller
 @RequestMapping("/fiches")
 public class FicheController {
@@ -30,9 +23,9 @@ public class FicheController {
 	@Autowired
 	private IPointageService pointageService;
 
-	@Autowired
-	@Qualifier("FichePointageHebdoReporting")
-	private IReporting fichePointageHebdoReporting;
+//	@Autowired
+//	@Qualifier("FichePointageHebdoReporting")
+//	private IReporting fichePointageHebdoReporting;
 	
 
 	@ResponseBody
@@ -46,21 +39,21 @@ public class FicheController {
 		return new ModelAndView("xmlView", "object", fiches);
 	}
 	
-	@ResponseBody
-	@RequestMapping(value = "/pdf/getFichesPointage", produces = "application/xml", method = RequestMethod.GET)
-	@Transactional(readOnly = true)
-	public ResponseEntity<String> getPdfFichesPointage(@RequestParam("csvIdAgents") String csvIdAgents,
-			@RequestParam("date") @DateTimeFormat(pattern = "yyyyMMdd") Date date) throws ParseException {
-
-		try {
-			fichePointageHebdoReporting.getFichePointageHebdoReporting();
-		} catch (DocumentException e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-		} catch (IOException e) {
-			return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
-		}
-
-
-		return new ResponseEntity<String>(HttpStatus.OK);
-	}
+//	@ResponseBody
+//	@RequestMapping(value = "/pdf/getFichesPointage", produces = "application/xml", method = RequestMethod.GET)
+//	@Transactional(readOnly = true)
+//	public ResponseEntity<String> getPdfFichesPointage(@RequestParam("csvIdAgents") String csvIdAgents,
+//			@RequestParam("date") @DateTimeFormat(pattern = "yyyyMMdd") Date date) throws ParseException {
+//
+//		try {
+//			fichePointageHebdoReporting.getFichePointageHebdoReporting();
+//		} catch (DocumentException e) {
+//			return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+//		} catch (IOException e) {
+//			return new ResponseEntity<String>(e.getMessage(), HttpStatus.CONFLICT);
+//		}
+//
+//
+//		return new ResponseEntity<String>(HttpStatus.OK);
+//	}
 }
